0e988ceab22fab26b0d5a86d59a8796d4a0452eb
prmidash <87124207+prmidash@users.noreply.github.com>
2021-07-13 11:57:16 +0430

    Update again.py

--- again.py
+++ again.py
@@ -3,53 +3,63 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 import fluids
-r={}
 
+m=0
 def start(update : Update , callback : CallbackContext):
-    update.message.reply_text("Hi! Please Enter your request:\n/Friction_factor\n/Reynolds\n/Help")
+    update.message.reply_text("Hi {}! Please Enter your request:\n/Friction_factor\n/Reynolds\n/Help".format(update.message.from_user.first_name))
 
 def Friction_factor(update : Update , callback : CallbackContext):
-    update.message.reply_text("Please Enter Reynold's number:")
+    update.message.reply_text("Please Enter Reynold's  and Relative roughness number like:\nR number,E number:")
+def Reynold(update : Update , callback : CallbackContext):
+    update.message.reply_text("please enter Density, Diameter, Velocity and Viscosity like\nD num,d num,v num,vis num")
+def F(update : Update , callback : CallbackContext):
     s=str(update.message)
-    if '.' in s:
-        s=s.split()
-        s.remove('.')
-        e=''
-        for o in s:
-            e+=o
-        if e.isdigit()==False:
-            update.message.reply_text("invalid number! please enter again:")
-    elif s.isdigit()==False:
-        update.message.reply_text("invalid number! please enter again:")
-    else:
-        Reynolds = update.message
-        update.message.reply_text("Please Enter relative roughness:")
-        s=str(update.message)
-        if '.' in s:
-            s=s.split()
-            s.remove('.')
+    s=s.split(',')
+    if len(s)==2:
+        R=s[0]
+        E=s[1]
+        R=R.split()
+        E=E.split()
+        if R[0]=='R' and E[0]=='E':
+            if '.' in R[1]:
+            R[1]=list(R[1])
+            R[1].remove('.')
             e=''
             for o in s:
-                e+=o
+                    e+=o
             if e.isdigit()==False:
                 update.message.reply_text("invalid number! please enter again:")
-        elif s.isdigit()==False:
-            update.message.reply_text("invalid number! please enter again:")
-        else:
-            roughness=update.message
-            m=2
-            if m==2:
-                f=fluids.friction.friction_factor(Reynolds,roughness)
-                update.message.reply_text(f)
-def hi(update : Update , callback : CallbackContext):
-    update.message.reply_text("hiii")
-
+            elif R[1].isdigit()==False:
+                update.message.reply_text("invalid number! please enter again:")
+            else:
+                Reynolds = float(R[1])
+                if '.' in E[1]:
+                    E[1]=list(E[1])
+                    E[1].remove('.')
+                    e=''
+                    for o in s:
+                        e+=o
+                    if e.isdigit()==False:
+                        update.message.reply_text("invalid number! please enter again:")
+                elif s.isdigit()==False:
+                    update.message.reply_text("invalid number! please enter again:")
+                else:
+                    roughness=float(s[1])
+                    f=fluids.friction.friction_factor(Reynolds,roughness)
+                    update.message.reply_text(f)
+    else:
+        r={}
+        for a in s:
+            a=a.split()
+            r[a[0]]=float(a[1])
+        re=((r['D'])*(r['d'])*(r['v']))/(r['vis'])  
+        update.message.reply_text(re)
 def main():
     updater = Updater("1716629236:AAF48G2vsOYNv_yPOJsUUAdajdtHInlQv0w")
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start" , start))
     dispatcher.add_handler(CommandHandler("Friction_factor" , Friction_factor))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command , hi))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command,F))
     updater.start_polling()
     updater.idle()
 
