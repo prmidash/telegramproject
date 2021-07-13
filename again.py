6e1ac88de0a096bcc1c047265e03ba2b5259c1c6
prmidash <87124207+prmidash@users.noreply.github.com>
2021-07-13 10:44:51 +0430

    Update again.py

--- again.py
+++ again.py
@@ -2,29 +2,54 @@ import logging
 from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
+import fluids
+r={}
 
 def start(update : Update , callback : CallbackContext):
-    update.message.reply_text("Hi! Please Enter your request:\n/Friction_factor\n/Show\n/Help")
+    update.message.reply_text("Hi! Please Enter your request:\n/Friction_factor\n/Reynolds\n/Help")
 
 def Friction_factor(update : Update , callback : CallbackContext):
-    update.message.reply_text("Please Enter Reynolds number")
-def get_Reynolds(update : Update , callback : CallbackContext):
-    Reynolds = update.message
-    update.message.reply_text("Please Enter relative roughness")
-def relative_roughness(update : Update , callback : CallbackContext):
-    roughness = update.message
-
-
-
-
-
+    update.message.reply_text("Please Enter Reynold's number:")
+    s=str(update.message)
+    if '.' in s:
+        s=s.split()
+        s.remove('.')
+        e=''
+        for o in s:
+            e+=o
+        if e.isdigit()==False:
+            update.message.reply_text("invalid number! please enter again:")
+    elif s.isdigit()==False:
+        update.message.reply_text("invalid number! please enter again:")
+    else:
+        Reynolds = update.message
+        update.message.reply_text("Please Enter relative roughness:")
+        s=str(update.message)
+        if '.' in s:
+            s=s.split()
+            s.remove('.')
+            e=''
+            for o in s:
+                e+=o
+            if e.isdigit()==False:
+                update.message.reply_text("invalid number! please enter again:")
+        elif s.isdigit()==False:
+            update.message.reply_text("invalid number! please enter again:")
+        else:
+            roughness=update.message
+            m=2
+            if m==2:
+                f=fluids.friction.friction_factor(Reynolds,roughness)
+                update.message.reply_text(f)
+def hi(update : Update , callback : CallbackContext):
+    update.message.reply_text("hiii")
 
 def main():
-    updater = Updater("1882082108:AAFyoRnZucW21ffK1Y3H-uTwwIO50xLXd-g")
+    updater = Updater("1716629236:AAF48G2vsOYNv_yPOJsUUAdajdtHInlQv0w")
     dispatcher = updater.dispatcher
     dispatcher.add_handler(CommandHandler("start" , start))
     dispatcher.add_handler(CommandHandler("Friction_factor" , Friction_factor))
-    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command , get_Reynolds))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command , hi))
     updater.start_polling()
     updater.idle()
 
