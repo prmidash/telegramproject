190622967b063c4b56789859cbb7e25670c36b35
prmidash <87124207+prmidash@users.noreply.github.com>
2021-07-14 12:04:38 +0430

    Update again.py

--- again.py
+++ again.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 import fluids
-
+import os
 m=0
 def start(update : Update , callback : CallbackContext):
     update.message.reply_text("Hi {}! Please Enter your request:\n/Friction_factor\n/Reynolds".format(update.message.from_user.first_name))
@@ -74,7 +74,7 @@ def main():
     dispatcher.add_handler(CommandHandler("Friction_factor" , Friction_factor))
     dispatcher.add_handler(CommandHandler("Reynolds" , Reynold))
     dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command,F))
-    updater.start_polling()
+    updater.start_webhook(listen="0.0.0.0",port=int(os.environ.get('PORT' , 5000)),url_path="1716629236:AAF48G2vsOYNv_yPOJsUUAdajdtHInlQv0w",webhook_url="https://fluid-mechanic.herokuapp.com/1716629236:AAF48G2vsOYNv_yPOJsUUAdajdtHInlQv0w")
     updater.idle()
 
 if __name__ == "__main__":
